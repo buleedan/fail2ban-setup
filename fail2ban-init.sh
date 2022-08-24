6d828b4ac2e1bf9b999e09afbed444c167900622
Robin Dutheil <robin.dutheil@gmail.com>
2022-08-24 15:00:43 +0700

    First commit

---
+++ fail2ban-init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+## Install Fail2ban
+
+## Add custom filters
+
+## Setup DB
+
+
+## Setup GeoIP DB
+sudo apt-get -y install geoip-bin geoip-database
+
+## Setup connection to DB
+
+
+## Custom local jail
