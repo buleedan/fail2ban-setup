9edd2bd180832068bba2b2112d2c209df9886b09
Robin Dutheil <robin.dutheil@gmail.com>
2022-08-25 20:53:52 +0700

    Change MYSQL query to provide the script source

--- fail2ban-init.sh
+++ fail2ban-init.sh
@@ -17,7 +17,9 @@ randomString() {
 ## Install Fail2ban
 ##
 echo -e "${LIGHTBLUE}INSTALL FAIL2BAN${NC}"
+echo -e "${LIGHTPURPLE}Update apt repo${NC}"
 sudo apt -y update
+echo -e "${LIGHTPURPLE}Install Fail2ban${NC}"
 sudo apt -y install fail2ban
 
 echo -e "${LIGHTPURPLE}✔ Fail2ban installed${NC}"
@@ -34,7 +36,6 @@ echo -e "${LIGHTPURPLE}✔ Custom filters added${NC}"
 ##
 echo -e "${LIGHTBLUE}SETUP DATABASE${NC}"
 
-DB_TEMPLATE=/tmp/fail2ban.mysql;
 DB_HOST=127.0.0.1
 DB_PORT=3306
 DATABASE="fail2ban"
@@ -42,8 +43,10 @@ DB_USER="fail2ban"
 DB_PASSWORD=$(randomString)
 
 cd /tmp
+echo -e "${LIGHTPURPLE}Download MYSQL database creation script${NC}"
 wget https://raw.githubusercontent.com/buleedan/fail2ban-setup/master/database/fail2ban.mysql
 
+echo -e "${LIGHTPURPLE}Create ${DATABASE} database and user${NC}"
 if [ -f /root/.my.cnf ]; then
   mysql -e "CREATE DATABASE ${DATABASE} DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
   mysql -e "GRANT ALL ON ${DATABASE}.* TO '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
@@ -55,7 +58,7 @@ else
   mysql -uroot -p${MYSQL_PSWD} -e "CREATE DATABASE ${DATABASE} DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
   mysql -uroot -p${MYSQL_PSWD} -e "GRANT ALL ON ${DATABASE}.* TO '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
   mysql -uroot -p${MYSQL_PSWD} -e "USE ${DATABASE};"
-  mysql -uroot -p${MYSQL_PSWD} -e "SOURCE ${DB_TEMPLATE};"
+  mysql -uroot -p${MYSQL_PSWD} -e "SOURCE /tmp/fail2ban.mysql;"
 fi
 
 echo -e "+"
@@ -81,13 +84,19 @@ echo -e "${LIGHTPURPLE}✔ GEOIP installed${NC}"
 ## Setup connection to DB
 ##
 echo -e "${LIGHTBLUE}SETUP CONNECTION TO DATABASE${NC}"
+echo -e "${LIGHTPURPLE}Download fail2ban configuration${NC}"
 wget https://raw.githubusercontent.com/buleedan/fail2ban-setup/master/database/banned_db.conf
+echo -e "${LIGHTPURPLE}Move banned_db.conf to the right directory${NC}"
 sudo mv banned_db.conf /etc/fail2ban/action.d/
 
+echo -e "${LIGHTPURPLE}Download fail2ban DB script${NC}"
 wget https://raw.githubusercontent.com/buleedan/fail2ban-setup/master/database/fail2ban_banned_db
+echo -e "${LIGHTPURPLE}Move fail2ban_banned_db to the right directory${NC}"
 sudo mv fail2ban_banned_db /usr/local/bin/
+echo -e "${LIGHTPURPLE}Set banned_db.conf CHMOD${NC}"
 sudo chmod 0550 /usr/local/bin/fail2ban_banned_db
 
+echo -e "${LIGHTPURPLE}Generate MYSQL connection configuration file for fail2ban${NC}"
 sudo cat >/root/.my.cnf-fail2ban << EOL
 [client]
 host="${DB_HOST}"
@@ -103,6 +112,7 @@ echo -e "${LIGHTPURPLE}✔ Connection to Database set${NC}"
 ##
 echo -e "${LIGHTBLUE}SETUP CUSTOM LOCAL JAIL${NC}"
 wget https://raw.githubusercontent.com/buleedan/fail2ban-setup/master/jail.d/jail.local
+echo -e "${LIGHTPURPLE}Move jail.local to the right directory${NC}"
 sudo mv jail.local /etc/fail2ban
 
 echo -e "${LIGHTPURPLE}✔ Custom local jail setup${NC}"
